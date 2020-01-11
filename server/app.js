564f5ac0124cce36974c4000be5ca682abaffd62
shannonjanehogan <shannon.jane.hogan@gmail.com>
2020-01-11 13:22:27 -0800

    Return 501 for request

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+  res.sendStatus(501);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
